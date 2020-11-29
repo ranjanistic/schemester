aefa7e789695d41ea7bf714e3028630da60be6d1
ranjanistic <priyanshuranjan88@gmail.com>
2020-11-30 03:59:14 +0530

    test: tests for few workers added
    Granular tests for a few workers added on npm test as well as npm run commit, thus tests will run
    before commit on npm run commit

---
+++ test/inspector.test.js
@@ -0,0 +1,44 @@
+const assert = require("assert");
+const { ObjectId } = require("mongodb");
+const {client} = require("../public/script/codes");
+describe("Inspector", () => {
+  const inspect = require("../workers/common/inspector");
+
+  describe("Email validity", () => {
+    it("Valid string", () => {
+      assert.strictEqual(inspect.emailValid("abc@def.com"), true);
+    });
+    it("Invalid string", () => {
+      assert.strictEqual(inspect.emailValid("invalidemail"), false);
+    });
+  });
+
+  describe("Password validity", () => {
+    it("Valid string", () => {
+      assert.strictEqual(inspect.passValid("validpassword"), true);
+    });
+    it("Invalid string", () => {
+      assert.strictEqual(inspect.passValid("abc"), false);
+    });
+  });
+
+  describe("Tokening", () => {
+    it("Signs & Verifies", () => {
+      let tok = inspect.token.sign("testtoken");
+      assert.strictEqual(inspect.token.verify(tok),"testtoken");
+    });
+  });
+
+  describe("Session Token", () => {
+    it("Valid token", () => {
+      assert.strictEqual(inspect.sessionTokenValid({id:new ObjectId(),uiid:"testuiid"}), inspect.sessionTokenValid({id:new ObjectId(),uiid:"testuiid",classname:"testclass"},client.student));
+    });
+
+    it("Invalid token", () => {
+      assert.strictEqual(inspect.sessionTokenValid({id:"invalidobjectid",uiid:"testuiid"}), false);
+    });
+  });
+  
+
+
+});
--- workers/common/timer.js
+++ workers/common/timer.js
@@ -3,117 +3,137 @@
  * @key SGT: Schemester Global Time: The global notation as YYYYMMDDHHmmSSmmm, in the order of year, month, day, hour, minutes, seconds, milliseconds.
  */
 class Time {
-  constructor() {}
+  constructor() {
+    this.keys = ['year','month','day','hour','minute','second','milli']
+  }
   /**
    * Returns days in given month of given year.
    * @param {Number} monthIndex The month index of which days to be returned (January = 0).
    * @param {Number} year The year in which the given month index is to be found.
    * @return {Number} Total days in given month index.
    */
-  daysInMonth = (monthIndex = Number, year = Number) =>
-    new Date(year, monthIndex, 0).getDate();
+  daysInMonth = (monthIndex = Number, year = new Date().getFullYear()) =>
+    new Date(year, monthIndex+1, 0).getDate();
 
   /**
+   * Returns the current moment (or incremented) in SGT notation.
+   * @param {JSON} increment int objects from any of ['year','month','day','hour','minute','second','milli'] for increment. If not provided, then no increment takes place.
+   * @param {Boolean} string Returns string if true, defaults to false (Number).
+   * @note The value returned could differ at two different instances because of the shift of milliseconds. Therefore, use a single instance of this method wherever possible.
+   */
+  getMoment(increment = {
+    year:0,
+    month:0,
+    day:0,
+    hour:0,
+    minute:0,
+    second:0,
+    milli:0
+  },string = false){
+    if(!increment) increment = {}
+    this.keys.forEach((k)=>{
+      increment[k] = increment[k]?increment[k]:0
+    });
+    let currentTime = new Date();
+    let currentOffset = currentTime.getTimezoneOffset();
+    let ISTOffset = 330;   // IST offset UTC +5:30 
+    let d = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
+    let year = d.getFullYear();
+    let month = d.getMonth();
+    let day = d.getDate();
+    let hour = d.getHours();
+    let min = d.getMinutes();
+    let sec = d.getSeconds();
+    let milli = d.getMilliseconds();
+
+    if(milli+increment.milli > 999){
+      milli = (milli+increment.milli)-1000;
+      sec++;
+      while(milli>999){
+        sec++;
+        milli-=1000;
+      }
+    } else milli += increment.milli;
+
+    if(sec+increment.second > 59){
+      sec = (sec+increment.second)-60;
+      min++;
+      while(sec>59){
+        min++;
+        sec-=60;
+      }
+    } else sec += increment.second;
+
+    if(min+increment.minute > 59){
+      min = (min+increment.minute) - 60;
+      hour++;
+      while(min>59){
+        hour++;
+        min-=60;
+      }
+    } else min += increment.minute;
+
+    if(hour+increment.hour>23){
+      hour = (hour+increment.hour)-24;
+      day++
+      while(hour>23){
+        day++;
+        hour -= 24;
+      }
+    } else hour += increment.hour;
+
+    if(day+increment.day>this.daysInMonth(month)){
+      day = (day+increment.day)-this.daysInMonth(month)
+      month++;
+      while(day>this.daysInMonth(month)){  
+        month++;
+        day -= this.daysInMonth(month);
+      }
+    } else day += increment.day;
+
+    month++;
+    if(month+increment.month>12){
+      month = month+increment.month -12
+      year++;
+      while(month>12){
+        year++;
+        month -= 12;
+      }
+    } else month += increment.month;
+
+    year+=increment.year;
+    return this.getTemplateNotation(year,month,day,hour,min,sec,milli,string);
+  }
+
+  getTemplateNotation(year,month,day,hour,minute,seconds,millis,string = false){
+    if(month<10) month = `0${month}`;
+    if(day<10) day = `0${day}`;
+    if(hour<10) hour = `0${hour}`;
+    if(minute<10) minute = `0${minute}`;
+    if(seconds<10) seconds = `0${seconds}`;
+    if(millis<10) millis = `00${millis}`;
+    else if(millis<100) millis = `0${millis}`;
+    return string?`${year}${month}${day}${hour}${minute}${seconds}${millis}`:Number(`${year}${month}${day}${hour}${minute}${seconds}${millis}`);
+  }
+
+  /**
+   * @deprecated As of November 30, 2020 03:30 IST
+   * @see Time.getMoment() method.
    * Returns the current moment in SGT notation.
    * @param {Boolean} stringForm Defaults to true. This ensures that returned time in SGT notation is of type string?true else type number?false.
    * @param {Number} dayincrement The number of days to be incremented in returned SGT time. Defaults to 0.
    */
-  getTheMoment = (stringForm = true, dayincrement = 0) => {
-    let d = new Date();
-    let year = d.getFullYear();
-    let month = d.getMonth() + 1;
-    let date = d.getDate();
-    let incrementedDate = date + dayincrement;
-    if (this.daysInMonth(month, year) - incrementedDate < 0) {
-      incrementedDate = incrementedDate - this.daysInMonth(month, year);
-      if (12 - (month + 1) < 0) {
-        month = 13 - month;
-        year++;
-      } else {
-        month++;
-      }
-    }
-    incrementedDate =
-      incrementedDate < 10 ? `0${incrementedDate}` : incrementedDate;
-    month = month < 10 ? `0${month}` : month;
-    let hour = d.getHours() < 10 ? `0${d.getHours()}` : d.getHours();
-    let min = d.getMinutes() < 10 ? `0${d.getMinutes()}` : d.getMinutes();
-    let insts = d.getSeconds();
-    let secs = insts < 10 ? `0${insts}` : insts;
-    let instm = d.getMilliseconds();
-    let milli = instm < 10 ? `00${instm}` : instm < 100 ? `0${instm}` : instm;
-    if (stringForm) {
-      return (
-        String(year) +
-        String(month) +
-        String(incrementedDate) +
-        String(hour) +
-        String(min) +
-        String(secs) +
-        String(milli)
-      );
-    } else {
-      return Number(
-        String(year) +
-          String(month) +
-          String(incrementedDate) +
-          String(hour) +
-          String(min) +
-          String(secs) +
-          String(milli)
-      );
-    }
-  };
-  /**
-   * Gets current date time in SGT numeric notation.
-   * @param {Number} minuteIncrement The number of minutes to be incremented in returned timing. (default=0)
-   * @returns {Number} A number showing the current/incremented time in the SGT notation.
-   */
-  getTheMomentMinute = (minuteIncrement = 0) => {
-    let d = new Date();
-    let year = d.getFullYear();
-    let month = d.getMonth() + 1;
-    let date = d.getDate();
-    let hour = d.getHours();
-    let min = d.getMinutes();
-    let incrementMin = min + minuteIncrement;
-    if (incrementMin > 59) {
-      incrementMin = incrementMin - 60;
-      if (24 < hour + 1) {
-        //if next hour is next day
-        date++;
-        if (time.daysInMonth(month, year) < date) {
-          date = date - time.daysInMonth(month, year);
-          if (12 < month + 1) {
-            month = 13 - month;
-            year++;
-          } else {
-            month++;
-          }
-        }
-      } else {
-        hour++;
-      }
-    }
-    month = month < 10 ? `0${month}` : month;
-    date = date < 10 ? `0${date}` : date;
-    hour = hour < 10 ? `0${hour}` : hour;
-    incrementMin = incrementMin < 10 ? `0${incrementMin}` : incrementMin;
+  getTheMoment = (stringForm = true, dayincrement = 0) => this.getMoment({day:dayincrement},stringForm);
 
-    let insts = d.getSeconds();
-    let secs = insts < 10 ? `0${insts}` : insts;
-    let instm = d.getMilliseconds();
-    let milli = instm < 10 ? `00${instm}` : instm < 100 ? `0${instm}` : instm;
-    return Number(
-      String(year) +
-        String(month) +
-        String(date) +
-        String(hour) +
-        String(incrementMin) +
-        String(secs) +
-        String(milli)
-    );
-  };
+  /**
+   * @deprecated As of November 30, 2020 03:30 IST
+   * @see Time.getMoment() method.
+   * Gets current date time in SGT notation.
+   * @param {Number} minuteIncrement The number of minutes to be incremented in returned timing. (default=0)
+   * @returns current/incremented time in the SGT notation.
+   */
+  getTheMomentMinute = (minuteIncrement = 0,stringform = true) => this.getMoment({minute:minuteIncrement},stringform);
+
 }
 
 module.exports = new Time();
