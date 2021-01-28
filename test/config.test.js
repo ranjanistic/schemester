3a2438b8b323d999ddd4c6dd2c8fb3ef307cf41b
ranjanistic <priyanshuranjan88@gmail.com>
2021-01-29 03:59:14 +0530

    Update workflow :5

--- test/config.test.js
+++
@@ -1,8 +0,0 @@
-const assert =require("assert");
-const config = require("../config/config.json");
-const {token} = require("../workers/common/inspector");
-describe("Config keys",()=>{
-    it("keys valid",()=>{
-        assert.strictEqual(Object.keys(config).length,7);
-    })
-})
