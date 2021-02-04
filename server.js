1751f071485a7519807a47ee1ce35c432de00b07
ranjanistic <priyanshuranjan88@gmail.com>
2021-02-05 01:49:18 +0530

    Create search path view

--- server.js
+++ server.js
@@ -40,6 +40,10 @@ mongo.connectToDB(require("./config/config.json").db.dpass,( err,dbname )=>{
   server.get(get.tour,(req,res)=>{
     render(res,view.tour,{filename:"slide",total:7});
   });
+  server.get(get.search,(req,res)=>{
+    render(res,view.search,{query:req.query.q,results:[]});
+  });
+
   server.get(get.offline,(_,res)=>{
     render(res,view.offline);
   });
